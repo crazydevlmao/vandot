b101682f37c5ede6974e06317bb3bb8b439e3660
crazydevlmao <balto.wrwnft@gmail.com>
2025-11-08 13:15:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // ==== EDIT THESE TWO LINES ====
-const TOKEN_MINT = "8BpzVsAyVTKmiy58Dk411oC9v3zErZbVk5buVTCiCqGL"; // <-- paste your mint
+const TOKEN_MINT = "3hUWEp91jBcgkg6uiKERGRPP6X1ojWsLH4G8AfU2MY5f"; // <-- paste your mint
 const SUPPLY = 1_000_000_000;              // <-- set your supply
 // ==============================
 
