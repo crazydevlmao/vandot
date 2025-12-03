3cd07d27e211da8c5db38df2cef5adc8212e76f8
crazydevlmao <balto.wrwnft@gmail.com>
2025-12-03 00:19:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ export default function VanSol() {
           onMouseLeave={() => { x.set(0); y.set(0); }}
         >
         <a
-  href="https://pump.fun/HwjjdT2mLDbTgm2ysKLTL82HckCyMYyJVzgRXHrtpArT"
+  href="https://pump.fun/FXqbuoF4Pg6wWSmeMP3Qp6WVy8fzivqxgSgZYT9AvW2S"
   target="_blank"
   rel="noopener noreferrer"
   className={`absolute top-4 right-4 px-3 py-1.5 rounded-full font-semibold text-sm transition hover:scale-110 ${
@@ -103,7 +103,7 @@ export default function VanSol() {
 
 
 
-          <a href="https://x.com/i/communities/1993795288172318805" target="_blank" rel="noopener noreferrer"
+          <a href="https://x.com/i/communities/1996084782480908743" target="_blank" rel="noopener noreferrer"
              className={`absolute top-4 left-4 p-2 rounded-full transition hover:scale-110 ${darkMode ? "bg-white text-black" : "bg-black text-white"}`}>
             <Twitter size={20} />
           </a>
@@ -113,7 +113,7 @@ export default function VanSol() {
 </h1>
 
 <p className={`text-lg italic mb-6 ${darkMode ? "text-gray-300" : "text-gray-700"}`}>
-  Evidence that imperfect humans existed.
+  Evidence that imperfect humans existed before the AI takeover. Built on solana.
 </p>
 
 <img
@@ -138,7 +138,7 @@ export default function VanSol() {
       </main>
 
       <footer className="w-full text-center py-6 text-sm z-[2]">
-        <span>HwjjdT2mLDbTgm2ysKLTL82HckCyMYyJVzgRXHrtpArT</span><span className="neon-pump font-bold"></span>
+        <span>FXqbuoF4Pg6wWSmeMP3Qp6WVy8fzivqxgSgZYT9AvW2S</span><span className="neon-pump font-bold"></span>
       </footer>
     </div>
   );
