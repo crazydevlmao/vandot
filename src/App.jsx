e12d45cbd92d9f11e1f44111aefe5be23bb7c42c
crazydevlmao <balto.wrwnft@gmail.com>
2025-12-06 23:20:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ export default function VanSol() {
           onMouseLeave={() => { x.set(0); y.set(0); }}
         >
         <a
-  href="https://pump.fun/"
+  href="https://pump.fun/4dHvWihpp7frXy9cZYqhPygZ6NP6sCp8vpEZT8jvpump"
   target="_blank"
   rel="noopener noreferrer"
   className={`absolute top-4 right-4 px-3 py-1.5 rounded-full font-semibold text-sm transition hover:scale-110 ${
@@ -138,7 +138,7 @@ export default function VanSol() {
       </main>
 
       <footer className="w-full text-center py-6 text-sm z-[2]">
-        <span>pump</span><span className="neon-pump font-bold"></span>
+        <span>4dHvWihpp7frXy9cZYqhPygZ6NP6sCp8vpEZT8jvpump</span><span className="neon-pump font-bold"></span>
       </footer>
     </div>
   );
